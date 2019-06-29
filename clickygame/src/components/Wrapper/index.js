4c5e92a9ff1fa4997b95c80f4d4a487aa2cee9eb
bcoggins78 <bcoggins78@gmail.com>
2019-06-29 02:23:08 -0400

    Added comments

--- clickygame/src/components/Wrapper/index.js
+++ clickygame/src/components/Wrapper/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./style.css";
 
+// Container that holds the PictureCards
 function Wrapper(props) {
   return (
-    <div className="container">
+    <div className="container card-holder">
       <div className="row">{props.children}</div>
     </div>
   )
