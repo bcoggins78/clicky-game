8e6ae47489ff97800dbe02fef45b38ed71e7347a
bcoggins78 <bcoggins78@gmail.com>
2019-06-29 02:25:44 -0400

    Added logo image, adjusted classes for styling

--- clickygame/src/components/NavBar/index.js
+++ clickygame/src/components/NavBar/index.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./style.css";
+import Logo from "./doctor-who-logo.jpg"
 
 function NavBar(props) {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="https://www.myclickygame.com">Doctor Who Clicky Game</a>
-                
+            <nav className="navbar navbar-expand-sm">
+                <img src={Logo} alt="Doctor Who" />
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav mx-auto">
-                        <span className="nav-item nav-link mx-auto">{props.status}</span>
-                        
-                        
+                        <span className="nav-item nav-link h5 status">{props.status}</span>
                     </div>
                     <div className="navbar-nav mx-right">
-                    <span className="nav-item nav-link">Score: {props.score} | Top Score: {props.totalScore}</span>
+                        <span className="nav-item nav-link h5 score">Score: {props.score} | Top Score: {props.totalScore}</span>
                     </div>
                 </div>
             </nav>
