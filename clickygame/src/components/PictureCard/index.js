0398153b5c8b5411369a557666f28e50cf75f907
bcoggins78 <bcoggins78@gmail.com>
2019-06-29 02:24:52 -0400

    Removed actor and name from the component, leaving on the image

--- clickygame/src/components/PictureCard/index.js
+++ clickygame/src/components/PictureCard/index.js
@@ -8,22 +8,16 @@ const styles = {
     picture: {
         height: "10rem"
     }
-
 }
 
 function PictureCard(props) {
     return (
-        
-            <div className="col-md-3" onClick={() => props.incrementScore(props.id)}>
-                <div className="card" style={styles.card}>
-                    <img style={styles.picture} src={props.image} className="card-img-top" alt={props.name} />
-                    <div className="card-body">
-                        {/* <h5 className="card-title">{props.name}</h5> */}
-                        <p className="card-text">Actor: {props.actor}</p>
-                    </div>
-                </div>
+        // Card that contain the props from the App component and uses an onClick event to pass the id to the incrementScore function
+        <div className="col-md-3" title={props.actor} onClick={() => props.incrementScore(props.id)}>
+            <div className="card" style={styles.card}>
+                <img style={styles.picture} src={props.image} className="card-img-top" alt={props.name} />
             </div>
-        
+        </div>
     )
 }
 
