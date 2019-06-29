0b98ea3514a6dcbe56d6105254d0a1e22e84fa72
bcoggins78 <bcoggins78@gmail.com>
2019-06-29 02:28:20 -0400

    Updated Footer component and added music player component for playing the Doctor Who theme

--- clickygame/src/components/Footer/index.js
+++ clickygame/src/components/Footer/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import AudioPlayer from "react-h5-audio-player";
+import Theme from "./theme.mp3"
 
-function Footer(props) {
+function Footer() {
     return (
-        <div>
-            This is the Footer
-        </div>
+        // Footer component that holds the music player component that plays the Doctor Who theme
+        <footer className="footer mt-auto py-3">
+            <div className="container">
+                <AudioPlayer
+                    src={Theme}
+                    onPlay
+                    loop  
+                />
+            </div>
+        </footer>
     )
 }
 
